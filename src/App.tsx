1a1642333fe1c631907683fc28de1f8d8e24c078
Juan Cruz <juan_pineda_12@hotmail.es>
2024-03-28 21:38:12 -0300

    commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { menuItems } from "./data/db";
+
+function App() {
+
+  return (
+    <>
+      <header className=" bg-slate-400 py-5 text-center">
+        <h1 className=" text-4xl font-black">Calculadora de Propinas y Consumo</h1>
+      </header>
+    </>
+  )
+}
+
+export default App
